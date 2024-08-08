36a17579797edb4ee4b00fd3d7ddfcb69979035c
Rachel Bricker <90290513+rbricke2@users.noreply.github.com>
2024-08-08 08:44:20 -0400

    Add files via upload

--- make_one_model.py
+++ make_one_model.py
@@ -29,8 +29,8 @@ with open(input_file) as pdb:
                     list_line[6:11]      = atom_serial_num_char.rjust(5)
                     new_line             = "".join(list_line)
                     output_file.write(new_line)
-		    atom_serial_num += 1
-		    output_file.write("TER      " + str(atom_serial_num) + "      UNK     2 ")
+                    atom_serial_num += 1
+                    output_file.write("TER      " + str(atom_serial_num) + "      UNK     2 \n")
                     continue
             output_file.write(line)
 
